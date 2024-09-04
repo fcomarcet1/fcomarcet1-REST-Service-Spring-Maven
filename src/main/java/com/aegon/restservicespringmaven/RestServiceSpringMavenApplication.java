6c3b0709a75f8cd1cb75a517a734bd54d7b79eb4
fcomarcet1 <fcomarcet1@gmail.com>
2024-09-04 09:55:25 +0200

    Add: Initialized spring project

---
+++ src/main/java/com/aegon/restservicespringmaven/RestServiceSpringMavenApplication.java
@@ -0,0 +1,13 @@
+package com.aegon.restservicespringmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceSpringMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestServiceSpringMavenApplication.class, args);
+    }
+
+}
